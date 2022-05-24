3552f615933c3c75107b03d67041b3d2cba8c562
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:36:21 +0800

    Create Manager.cpp

---
+++ Manager.cpp
@@ -0,0 +1,21 @@
+#include "Manager.h"
+
+#include <iostream>
+
+Manager::Manager(int id, std::string name, int age, int D_id) {
+  this->_Id = id;
+  this->_Name = name;
+  this->_Age = age;
+  this->_DepartmentID = D_id;
+}
+
+std::string Manager::getDepartName() { return "Manager"; }
+int Manager::WorkTime() {return 9;}
+void Manager::showInformation() {
+  std::cout << "Number: " << this->_Id << "\tName: " << this->_Name
+            << "\tAge: " << this->_Age
+            << "\tName of the post: " << this->getDepartName()
+            << "\tPost ID: " << this->_DepartmentID
+            << "\tJob: Assign tasks to employees." 
+            << "\tTime hours: "<<this->WorkTime()<<" hours." <<std::endl;
+}
