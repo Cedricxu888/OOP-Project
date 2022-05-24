c7551cd8c18c5566db6590edee978aa7396bcada
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:38:59 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Person.h"
+#include "Employee.h"
+#include "Management.h"
+#include "Manager.h"
+
+int main() {
+  Management m1; // create an variable named m1
+  m1.Run_System(); // use the Run_system function here
+  return 0;
+}
