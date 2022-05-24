1c22b2763a3f60ef4cb9abeb0e2d48345e08ad5d
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:35:56 +0800

    Create Manager.h

---
+++ Manager.h
@@ -0,0 +1,17 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include "Person.h"
+
+// Management personnel class
+class Manager : public Person {
+ public:
+  Manager(int id, std::string name,
+          int age, int D_id);  // constructor with three basic parameters
+  virtual std::string getDepartName() override;  // show job title
+  virtual void showInformation()
+      override;  // display the information of manager
+  virtual int WorkTime() override; // The number of hours worked per day by an manager
+};
+
+#endif
