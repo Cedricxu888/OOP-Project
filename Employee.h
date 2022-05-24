3a4e6e8bc479d0779120a0ebb3b5afcf2bd22c88
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:35:13 +0800

    Create Employee.h

---
+++ Employee.h
@@ -0,0 +1,18 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include "Person.h"
+// general employee class
+class Employee : public Person {
+ public:
+  Employee(int id, std::string name, int age, int D_id);
+  virtual std::string getDepartName()
+      override;  // override the worker class's function of the same name and
+                 // get the department name of the employee
+  virtual void showInformation()
+      override;  // override the worker class's function of the same name and
+                 // let the general employee class display the information
+  virtual int WorkTime() override; // The number of hours worked per day by an employee
+};
+
+#endif
