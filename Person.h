23278e5cd9c334a027e6fffe00808f6f80551106
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-05-24 16:34:22 +0800

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,17 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person {
+ public:
+ virtual std::string getDepartName() = 0;  // show job title
+ virtual void showInformation() = 0;       // display information of persons
+ virtual int WorkTime();                   // person's working time range
+ int _Id;                                  // individual numbers of persons
+ std::string _Name;                        // name of persons
+ int _Age;                                 // Age of persons
+ int _DepartmentID;
+};
+
+#endif
