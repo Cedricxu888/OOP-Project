c0acc19ff582d986a09e94b6cb3cfbf2d17c0520
Cedricxu888 <100258580+Cedricxu888@users.noreply.github.com>
2022-06-03 12:56:49 +0800

    Create BarnesNoble.h

---
+++ BarnesNoble.h
@@ -0,0 +1,22 @@
+#ifndef BARNESNOBLE_H
+#define BARNESNOBLE_H
+
+#include "BookStore.h"
+
+class BarnesNoble : public BookStore {
+
+    public:
+        BarnesNoble();          // inherit from base class default constructor    
+        BarnesNoble(string n);  // set name n  
+                       // should call the corresponding constructor from the parent class
+                       // with the online parameter being false 
+        void soldBook();   // increments numSoldBooks
+        int get_numSoldBooks(); // returns the books sold
+        void set_name(string Name);
+        string get_name();
+    protected:
+        int numSoldBooks;   // attribute for numSoldBooks, initialised to 0
+        string name;
+};
+
+#endif
